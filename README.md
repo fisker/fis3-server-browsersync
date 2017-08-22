afa13e96821dd484a437e65c58dd98598d4ce3ea
fisker <lionkay@gmail.com>
2017-08-22 17:47:53 +0800

    support bs-config.js

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-### fis3-server-browsersync
+## fis3-server-browsersync
 
-fis3 调试服务器。基于 browser-sync.
+fis3 debug server based on browser-sync.
+
+## ustage
+```sh
+fis3 server start --type browsersync
+```
+
+## custom config
+put `bs-config.js` in the source folder, it will automatic load
+
+OR
+
+fis3 server start --type browsersync --bs-config path/to/bs-config.js
+
+see https://browsersync.io/docs/options
--- app.js
+++ app.js
@@ -1,43 +1,100 @@
-
 var path = require('path');
+var fs = require('fs');
 var args = process.argv.join('|');
 var port = /\-\-port\|(\d+)(?:\||$)/.test(args) ? ~~RegExp.$1 : 8080;
 var https = /\-\-https\|(true)(?:\||$)/.test(args) ? !!RegExp.$1 : false;
-var DOCUMENT_ROOT = path.resolve(/\-\-root\|(.*?)(?:\||$)/.test(args) ? RegExp.$1 : process.cwd());
-var bsConfigFile = /\-\-bs\-config\|(.*?)(?:\||$)/.test(args) ? RegExp.$1 : false;
+var DOCUMENT_ROOT = path.resolve(
+  /\-\-root\|(.*?)(?:\||$)/.test(args) ? RegExp.$1 : process.cwd()
+);
+var bsConfigFile = /\-\-bs\-config\|(.*?)(?:\||$)/.test(args) ? RegExp.$1 : '';
 var bs = require('browser-sync').create();
-var bsConfig = {
-  notify: false
-};
+var chokidar = require('chokidar');
+var bsUtils = bs.instance.utils;
+var bsDefaultConfig = require('./node_modules/browser-sync/lib/default-config.js');
 
-if (bsConfigFile) {
-  var maybeconf = path.resolve(process.cwd(), bsConfigFile);
+function getUserConfig(path) {
   try {
-    var conf = require(maybeconf);
-    Object.assign(bsConfig, conf);
-  } catch(_) {}
+    var userConfig = require(path);
+    if (bsUtils.verifyConfig(userConfig)) {
+      return userConfig;
+    }
+  } catch (_) {}
 }
 
-Object.assign(bsConfig, {
-  server: {
-    baseDir: DOCUMENT_ROOT,
-    directory: true,
-  },
-  port: port,
-  https: https ? {
-    key: 'key.pem',
-    cert: 'cert.pem'
-  } : false,
-  open: false
+var userConfigFile = path.resolve(
+  process.cwd(),
+  bsConfigFile || bs.instance.config.userFile
+);
+
+
+if (fs.existsSync(userConfigFile)) {
+  // TODO:  watch bs-config.js
+  // var watcher = chokidar.watch(userConfigFile);
+  // watcher.on('change', startServer);
+  // watcher.on('unlink', startServer);
+  // watcher.on('add', startServer);
+}
+
+function getConfig() {
+  return Object.assign(
+    {},
+    bsDefaultConfig,
+    {
+      watchEvents: [
+        'change',
+        'add',
+        'addDir',
+        'unlink',
+        'unlinkDir'
+      ],
+      ghostMode: false,
+      reloadDebounce: 500,
+      notify: false,
+    },
+    getUserConfig(),
+    {
+      server: {
+        baseDir: DOCUMENT_ROOT,
+        directory: true
+      },
+      port: port,
+      https: https
+        ? {
+            key: 'key.pem',
+            cert: 'cert.pem'
+          }
+        : false,
+      open: false
+    }
+  );
+}
+
+function startServer() {
+  var bsConfig = getConfig();
+
+  bs.exit();
+  bs.init(bsConfig, function() {
+    console.log(
+      'Listening on ' + (https ? 'https' : 'http') + '://127.0.0.1:%d',
+      port
+    );
+  });
+
+  bs.watch(
+    path.join(DOCUMENT_ROOT, '**/*'),
+    {
+      ignored: path.join(DOCUMENT_ROOT, '**/*.log')
+    },
+    function(event, path) {
+      console.log(new Date().toJSON() + ' ' + event + ': ' + path);
+      bs.reload(path);
+    }
+  );
+}
+
+process.on('SIGTERM', function() {
+  console.log(' Recive quit signal in worker %s.', process.pid);
+  bs.exit();
 });
 
-bs.init(bsConfig, function() {
-  console.log('Listening on ' + (https ? 'https' : 'http') + '://127.0.0.1:%d', port);
-});
-
-bs.watch(path.join(DOCUMENT_ROOT, '**/*'), {
-  ignored: path.join(DOCUMENT_ROOT, '**/*.log')
-}, function (event, path) {
-  console.log(new Date().toJSON() + ' ' + event + ': ' + file);
-  bs.reload(path);
-});
+startServer();
