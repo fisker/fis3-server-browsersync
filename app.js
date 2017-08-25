46c75109af0e06ca571896a5c5bc7576b3c2112d
fisker <lionkay@gmail.com>
2017-08-25 18:51:15 +0800

    custom index

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var DOCUMENT_ROOT = path.resolve(
   /\-\-root\|(.*?)(?:\||$)/.test(args) ? RegExp.$1 : process.cwd()
 );
 var bsConfigFile = /\-\-bs\-config\|(.*?)(?:\||$)/.test(args) ? RegExp.$1 : '';
-var serveIndex = require('serve-index');
+var serveDirectory = require('serve-directory');
 var bs = require('browser-sync').create();
 var bsUtils = bs.instance.utils;
 var bsDefaultConfig = require(path.join(path.dirname(require.resolve('browser-sync')),'./lib/default-config.js'));
@@ -24,12 +24,34 @@ var userConfigFile = path.resolve(
   bsConfigFile || bs.instance.config.userFile
 );
 
+var serveDirectoryStyle = fs.readFileSync(path.join(__dirname, './public/style.css'), 'utf-8');
+var serveDirectoryTemplate = fs.readFileSync(path.join(__dirname, './public/directory.html'), 'utf-8');
+
+var iconColor = '#6a737d';
+var icons = {
+  directory: '<svg xmlns="http://www.w3.org/2000/svg" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M13 4H7V3c0-.66-.31-1-1-1H1c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V5c0-.55-.45-1-1-1zM6 4H1V3h5v1z"></path></svg>',
+  file: '<svg xmlns="http://www.w3.org/2000/svg" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M6 5H2V4h4v1zM2 8h7V7H2v1zm0 2h7V9H2v1zm0 2h7v-1H2v1zm10-7.5V14c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1V2c0-.55.45-1 1-1h7.5L12 4.5zM11 5L8 2H1v12h10V5z"></path></svg>'
+};
+
+var btoa = global.btoa || function(str) {
+  return new Buffer(str).toString('base64');
+};
+
+function makeSvg(svg) {
+  return 'url(data:image/svg+xml;base64,' + btoa(svg.replace('<svg ', '<svg style="fill:#6a737d" ')) +')';
+  // ie can't recognize
+  // return 'url(data:image/svg+xml;utf8,' + encodeURIComponent(svg.replace('<svg ', '<svg style="fill:#6a737d" ')) +')';
+}
+
+icons.directory = makeSvg(icons.directory);
+icons.file = makeSvg(icons.file);
+
 var serveIndexMiddleWare = {
   route: '',
-  handle: serveIndex(DOCUMENT_ROOT, {
-    icons: true,
-    stylesheet: 'public/style.css',
-    template: 'public/directory.html'
+  handle: serveDirectory(DOCUMENT_ROOT, {
+    template: serveDirectoryTemplate,
+    style: serveDirectoryStyle,
+    icons: icons
   }),
   id: 'Browsersync Server Directory Middleware'
 };
@@ -49,6 +71,7 @@ function getConfig() {
       ghostMode: false,
       reloadDebounce: 500,
       notify: false,
+      online: false
     },
     getUserConfig(userConfigFile),
     {
